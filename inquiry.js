83d26cb18f8fec7ad86bef7cc420a988746ad96e
merlink1111 <148764356+merlink1111@users.noreply.github.com>
2023-12-17 18:18:46 +0800

    Delete inquiry.js

--- inquiry.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("contact-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    // Assuming you want to display a thank you message and hide the form
-    document.getElementById("contact-form").style.display = "none";
-    document.getElementById("thank-you").style.display = "block";
-});
